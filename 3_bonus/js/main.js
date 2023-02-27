c6f2f527c996848fc11d95b5df02cf09b2707b8c
RicchiutiLucia <luciaricchiuti04@gmail.com>
2023-02-27 18:36:49 +0100

    correzione e fine bonus

--- 3_bonus/js/main.js
+++ 3_bonus/js/main.js
@@ -4,8 +4,24 @@ const btnDom = document.getElementById('btn');
 
 const elencoEmailDom = document.getElementById('elenco-email');
 
+const arrayMail =['pippo@gmail.com', 'pluto@gmail.com', 'paperino@libero.it', 'topolino@yahoo.it'];
+
 btnDom.addEventListener('click',
     function(){
-        elencoEmailDom.innerHTML= mailDom.value;
+        let emails= false;
+        for(let i = 0; i < arrayMail.length; i++){
+            if((mailDom.value).toLowerCase() == arrayMail[i] ){
+                console.log('Email è gia esistente');
+                elencoEmailDom.innerHTML = "La mail che hai inserito è stata validata correttamente";
+                emails = true;
+                i=arrayMail.length;
+            }
+        }
+
+        if(!emails ){
+            console.log('Email inserita correttamente');
+            elencoEmailDom.innerHTML = "La mail che hai inserito non è presente nella nostra lista";
+        }
     }
+   
 )
